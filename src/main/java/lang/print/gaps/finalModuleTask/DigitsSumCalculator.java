d1a80f6aefbb56603c2e71ee0802412777304b2d
Akzhol <cheder911@gmail.com>
2023-04-14 22:33:10 +0600

    1

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = number%10;
+        number = number - sum;
+        sum = sum + number%100;
+        number = number - sum;
+        sum = sum + number%1000;
+        number = number - sum;
+        sum = sum + number;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int reminder = number%10;
+        int reversed = reminder;
+        number = number - reminder;
+        reminder = number%100;
+        reversed = reversed + 100*reminder;
+        number = number - reminder;
+        reminder = number/100;
+        reversed = reversed + reminder;
+        System.out.println(reversed);
     }
 }
